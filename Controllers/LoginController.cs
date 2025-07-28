3198a989201447f69e7914733c81a7effaa632b8
SyndoriaMiku <61247021+SyndoriaMiku@users.noreply.github.com>
2025-07-28 14:52:30 +0700

    add login views

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,6 +1,28 @@
+
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
 namespace WebApplication1.Controllers;
 
-public class LoginController
+public class LoginController : Controller
 {
+    private readonly ILogger<LoginController> _logger;
     
+    public LoginController(ILogger<LoginController> logger)
+        {
+        _logger = logger;
+        }
+    
+    
+    [HttpGet("/login")]
+    public IActionResult Index()
+    {
+        return View("~/Views/Home/Login.cshtml");
+    }
+    
+    [HttpPost]
+    public IActionResult Index(string username, string password)
+    {
+        return RedirectToAction("Index", "Home");
+    }
 }
